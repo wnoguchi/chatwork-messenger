a22d985bdc37d979c93a69f1af214777fb440a38
Wataru Noguchi <wnoguchi.0727@gmail.com>
2014-01-30 00:23:51 +0900

    Add Base Chatwork Form Sample.

---
+++ ChatWorkMessenger/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace ChatWorkMessenger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sendButton = new System.Windows.Forms.Button();
+            this.messageTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // sendButton
+            // 
+            this.sendButton.Location = new System.Drawing.Point(12, 220);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(75, 23);
+            this.sendButton.TabIndex = 0;
+            this.sendButton.Text = "送信";
+            this.sendButton.UseVisualStyleBackColor = true;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
+            // 
+            // messageTextBox
+            // 
+            this.messageTextBox.Location = new System.Drawing.Point(12, 12);
+            this.messageTextBox.Multiline = true;
+            this.messageTextBox.Name = "messageTextBox";
+            this.messageTextBox.Size = new System.Drawing.Size(384, 202);
+            this.messageTextBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(408, 255);
+            this.Controls.Add(this.messageTextBox);
+            this.Controls.Add(this.sendButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button sendButton;
+        private System.Windows.Forms.TextBox messageTextBox;
+    }
+}
+
---
+++ ChatWorkMessenger/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Windows.Forms;
+
+namespace ChatWorkMessenger
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void sendButton_Click(object sender, EventArgs e)
+        {
+            // 各種設定値
+            const string apiKey = "9999999999999999999999999999";
+            // グループチャットを指定
+            const string roomId = "9999999";
+
+            //文字コードを指定する
+            var enc = Encoding.GetEncoding("UTF-8");
+
+            // パラメタのエンコード・構築
+            var postData = "body=" + HttpUtility.UrlEncode(messageTextBox.Text, enc);
+            var postDataBytes = System.Text.Encoding.ASCII.GetBytes(postData);
+
+            // WebRequest作成
+            var requestUrl = string.Format("https://api.chatwork.com/v1/rooms/{0}/messages", roomId);
+            var req = WebRequest.Create(requestUrl);
+            req.Method = "POST";
+            req.ContentType = "application/x-www-form-urlencoded";
+            // POSTデータ長を指定
+            req.ContentLength = postDataBytes.Length;
+            req.Headers.Add(string.Format("X-ChatWorkToken: {0}", apiKey));
+
+            // データをPOST送信するためのStreamを取得
+            var reqStream = req.GetRequestStream();
+            // 送信するデータを書き込む
+            reqStream.Write(postDataBytes, 0, postDataBytes.Length);
+            reqStream.Close();
+
+            // サーバーからの応答を受信する
+            var res = req.GetResponse();
+            // 応答データを受信するためのStreamを取得
+            var resStream = res.GetResponseStream();
+            // 受信して表示
+            var sr = new StreamReader(resStream, enc);
+
+            // 結果受信
+            var responseMessage = sr.ReadToEnd();
+            
+            sr.Close();
+
+            MessageBox.Show(responseMessage);
+
+        }
+    }
+}
