fcd5d1a37e6bde76bca7bdaa78dc330e2402644c
Wataru Noguchi <wnoguchi.0727@gmail.com>
2014-02-05 00:00:39 +0900

    Implement rooms get call.

--- ChatWorkMessenger/Form1.cs
+++ ChatWorkMessenger/Form1.cs
@@ -80,6 +80,46 @@ namespace ChatWorkMessenger
         private void button1_Click(object sender, EventArgs e)
         {
 
+            // 各種設定値
+            var apiKey = _chatworkCredential.ApiKey;
+
+            // 文字コードを指定する
+            var enc = Encoding.GetEncoding("UTF-8");
+
+            // パラメタのエンコード・構築
+            var postData = "body=" + HttpUtility.UrlEncode(messageTextBox.Text, enc);
+            var postDataBytes = Encoding.ASCII.GetBytes(postData);
+
+            // WebRequest作成
+            var requestUrl = string.Format("https://api.chatwork.com/v1/rooms");
+            var req = WebRequest.Create(requestUrl);
+            req.Method = "GET";
+            req.ContentType = "application/x-www-form-urlencoded";
+            // POSTデータ長を指定
+//            req.ContentLength = postDataBytes.Length;
+            req.Headers.Add(string.Format("X-ChatWorkToken: {0}", apiKey));
+
+            // データをPOST送信するためのStreamを取得
+            
+//            var reqStream = req.GetRequestStream();
+            // 送信するデータを書き込む
+//            reqStream.Write(postDataBytes, 0, postDataBytes.Length);
+//            reqStream.Close();
+
+            // サーバーからの応答を受信する
+            var res = req.GetResponse();
+            // 応答データを受信するためのStreamを取得
+            var resStream = res.GetResponseStream();
+            // 受信して表示
+            var sr = new StreamReader(resStream, enc);
+
+            // 結果受信
+            var responseMessage = sr.ReadToEnd();
+
+            sr.Close();
+
+            MessageBox.Show(responseMessage);
+
         }
 
     }
